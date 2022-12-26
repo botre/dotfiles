c3f39f06c887080cc49cdfea96b1d057757ed198
botre <git@bjornkrols.com>
2022-12-26 08:41:02 +0100

    LSP support and remaps

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -40,8 +40,31 @@ require('packer').startup(function(use)
     use { 'nvim-telescope/telescope.nvim', tag = '0.1.0' }
     use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
     use { 'tpope/vim-commentary' }
+    use { 'tpope/vim-eunuch' }
+    use { 'tpope/vim-fugitive' }
     use { 'tpope/vim-surround' }
     use { 'preservim/nerdtree' }
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        requires = {
+            -- LSP support
+            { 'neovim/nvim-lspconfig' },
+            { 'williamboman/mason.nvim' },
+            { 'williamboman/mason-lspconfig.nvim' },
+
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' },
+            { 'hrsh7th/cmp-buffer' },
+            { 'hrsh7th/cmp-path' },
+            { 'saadparwaiz1/cmp_luasnip' },
+            { 'hrsh7th/cmp-nvim-lsp' },
+            { 'hrsh7th/cmp-nvim-lua' },
+
+            -- Snippets
+            { 'L3MON4D3/LuaSnip' },
+            { 'rafamadriz/friendly-snippets' },
+        }
+    }
 
     if installed_packer then
         require('packer').sync()
@@ -62,14 +85,21 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     end,
 })
 
--- Telescope mappings
+-- File utilities
+vim.keymap.set('n', '<leader>rf', ':Rename ', {})
+
+-- Git decorations
+require('gitsigns').setup()
+
+-- Telescope
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fu', builtin.lsp_references, {})
 
--- Treesitter configuration
+-- Treesitter
 require('nvim-treesitter.configs').setup({
-    ensure_installed = "all",
+    ensure_installed = 'all',
     sync_install = false,
     auto_install = true,
     highlight = {
@@ -77,3 +107,17 @@ require('nvim-treesitter.configs').setup({
         additional_vim_regex_highlighting = false,
     },
 })
+
+-- LSP
+local lsp = require('lsp-zero')
+
+lsp.preset('recommended')
+
+lsp.on_attach(function(_, bufnr)
+    local options = { buffer = bufnr, remap = false }
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, options)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, options)
+    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, options)
+end)
+
+lsp.setup()
