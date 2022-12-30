450901b04d7c3d07a6944d74f741bbde3ac28798
botre <git@bjornkrols.com>
2022-12-30 06:58:25 +0100

    Improved cmp mappings

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -144,6 +144,21 @@ local lsp = require('lsp-zero')
 
 lsp.preset('recommended')
 
+local cmp = require('cmp')
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
+local cmp_mappings = lsp.defaults.cmp_mappings({
+    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    ['<C-Space>'] = cmp.mapping.complete(),
+})
+lsp.setup_nvim_cmp({
+    completion = {
+        autocomplete = false
+    },
+    mapping = cmp_mappings
+})
+
 lsp.on_attach(function(_, bufnr)
     local options = { buffer = bufnr, remap = false }
     -- Format the buffer
