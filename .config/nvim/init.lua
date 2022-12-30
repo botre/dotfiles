472c575d4a94bb51433a24a7201112725a4458c6
botre <git@bjornkrols.com>
2022-12-30 06:44:13 +0100

    Sort

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -62,16 +62,16 @@ require('packer').startup(function(use)
         requires = {
             -- LSP support
             { 'neovim/nvim-lspconfig' },
-            { 'williamboman/mason.nvim' },
             { 'williamboman/mason-lspconfig.nvim' },
+            { 'williamboman/mason.nvim' },
 
             -- Autocompletion
-            { 'hrsh7th/nvim-cmp' },
             { 'hrsh7th/cmp-buffer' },
-            { 'hrsh7th/cmp-path' },
-            { 'saadparwaiz1/cmp_luasnip' },
             { 'hrsh7th/cmp-nvim-lsp' },
             { 'hrsh7th/cmp-nvim-lua' },
+            { 'hrsh7th/cmp-path' },
+            { 'hrsh7th/nvim-cmp' },
+            { 'saadparwaiz1/cmp_luasnip' },
 
             -- Snippets
             { 'L3MON4D3/LuaSnip' },
