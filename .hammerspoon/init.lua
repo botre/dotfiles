02e7d4bc90c73b1180d4cd8bf421d626ae415ba3
botre <git@bjornkrols.com>
2023-03-10 11:07:55 +0100

    Better Hyper mappings

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,21 +1,25 @@
 local am = require('app-management')
 
--- Hyper
+--[[ Hyper ]]
 local hyper = require('hyper')
 hyper.install('F18')
----- Browser
+-- Browser
 hyper.bindKey('b', function()
     am.switchToAndFromApp("com.google.chrome")
 end)
----- Finder
+-- Code editor
+hyper.bindKey('c', function()
+    am.switchToAndFromApp("com.jetbrains.intellij")
+end)
+-- Finder
 hyper.bindKey('f', function()
     am.switchToAndFromApp("com.apple.finder")
 end)
----- IDE
-hyper.bindKey('i', function()
-    am.switchToAndFromApp("com.jetbrains.intellij")
+-- Music
+hyper.bindKey('m', function()
+    am.switchToAndFromApp("com.spotify.client")
 end)
----- Terminal
+-- Terminal
 hyper.bindKey('t', function()
     am.switchToAndFromApp("org.alacritty")
 end)
