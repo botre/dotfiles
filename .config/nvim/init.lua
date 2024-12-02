c0cc78850c66a4b7061ebeb9266bcc2e8d9707c1
botre <git@bjornkrols.com>
2024-12-02 20:40:12 +0100

    Oil config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -63,14 +63,14 @@ require('packer').startup(function(use)
             'nvim-tree/nvim-web-devicons',
         },
         config = function()
-            require('oil').setup({
+            require('oil').setup {
                 columns = { 'icon' },
                 view_options = {
                     show_hidden = true,
                 }
-            })
-            vim.keymap.set('n', '-', '<CMD>Oil<CR>', {})
-            vim.keymap.set('n', '<leader>-', require('oil').toggle_float, {})
+            }
+            vim.keymap.set('n', '-', '<CMD>Oil<CR>')
+            vim.keymap.set('n', '<leader>-', require('oil').toggle_float)
         end
     }
     use { 'tpope/vim-commentary' }
