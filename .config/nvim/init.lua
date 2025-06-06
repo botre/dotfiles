21efada78df5747d1b1becd3a6ed3f2230331e5a
botre <git@bjornkrols.com>
2025-06-06 13:34:21 +0200

    Dash

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -233,7 +233,7 @@ require('mason-null-ls').setup {
     }
 }
 
-local null_ls = require('null_ls')
+local null_ls = require('null-ls')
 null_ls.setup({
     sources = {
         null_ls.builtins.formatting.black,
