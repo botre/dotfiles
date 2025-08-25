1858ba63c387b1a135dd48963e1f3ea061bec3af
botre <git@bjornkrols.com>
2025-08-25 14:03:16 +0200

    Remove cmd

--- .config/nvim/lua/plugins/noice.lua
+++ .config/nvim/lua/plugins/noice.lua
@@ -9,7 +9,6 @@ return {
         require("noice").setup({
             presets = {
                 bottom_search = true,
-                command_palette = true,
                 inc_rename = false,
                 long_message_to_split = true,
                 lsp_doc_border = false,
