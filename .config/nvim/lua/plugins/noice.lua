eade13cd8192803d851d07fbee31c38906ad9592
botre <git@bjornkrols.com>
2025-08-25 13:57:00 +0200

    Remove overrides

--- .config/nvim/lua/plugins/noice.lua
+++ .config/nvim/lua/plugins/noice.lua
@@ -5,15 +5,8 @@ return {
     dependencies = {
         "MunifTanjim/nui.nvim",
     },
-    config = function(_, opts)
+    config = function()
         require("noice").setup({
-            lsp = {
-                override = {
-                    ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-                    ["vim.lsp.util.stylize_markdown"] = true,
-                    ["cmp.entry.get_documentation"] = true,
-                },
-            },
             presets = {
                 bottom_search = true,
                 command_palette = true,
