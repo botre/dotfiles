a5618fec015a1b32f061b3d88a6077281ed49b73
botre <git@bjornkrols.com>
2025-10-05 13:07:31 +0200

    Extract copilot

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -139,26 +139,4 @@ return {
             })
         end,
     },
-
-    -- GitHub Copilot
-    {
-        'zbirenbaum/copilot.lua',
-        dependencies = {
-            'copilotlsp-nvim/copilot-lsp'
-        },
-        cmd = 'Copilot',
-        config = function()
-            require('copilot').setup({
-                suggestion = { enabled = false },
-                panel = { enabled = false },
-            })
-        end,
-    },
-    {
-        'zbirenbaum/copilot-cmp',
-        dependencies = { 'zbirenbaum/copilot.lua' },
-        config = function()
-            require('copilot_cmp').setup()
-        end,
-    },
 }
