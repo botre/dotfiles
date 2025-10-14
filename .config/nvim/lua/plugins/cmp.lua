1314967bcd9931ecea109a0b84505e91fa71266b
botre <git@bjornkrols.com>
2025-10-14 20:56:45 +0200

    Tweak cmp keymap

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -7,7 +7,20 @@ return {
         },
         version = '1.*',
         opts = {
-            keymap = { preset = 'default' },
+            keymap = {
+                preset = 'none',
+                ['<C-space>'] = { 'show', 'hide' },
+                ['<C-e>'] = { 'hide', 'fallback' },
+                ['<CR>'] = { 'accept', 'fallback' },
+                ['<Tab>'] = { 'select_next', 'snippet_forward', 'fallback' },
+                ['<S-Tab>'] = { 'select_prev', 'snippet_backward', 'fallback' },
+                ['<Up>'] = { 'select_prev', 'fallback' },
+                ['<Down>'] = { 'select_next', 'fallback' },
+                ['<C-p>'] = { 'select_prev', 'fallback' },
+                ['<C-n>'] = { 'select_next', 'fallback' },
+                ['<C-u>'] = { 'scroll_documentation_up', 'fallback' },
+                ['<C-d>'] = { 'scroll_documentation_down', 'fallback' },
+            },
             appearance = {
                 nerd_font_variant = 'mono'
             },
