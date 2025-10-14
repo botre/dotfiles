31fb37c7e699dea5ad044ed58f68b9c8c7a26fac
botre <git@bjornkrols.com>
2025-10-14 21:49:12 +0200

    Removes copilot cmp

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -3,7 +3,6 @@ return {
         'saghen/blink.cmp',
         dependencies = {
             'rafamadriz/friendly-snippets',
-            'fang2hou/blink-copilot',
         },
         version = '1.*',
         opts = {
@@ -31,15 +30,7 @@ return {
                 documentation = { auto_show = true }
             },
             sources = {
-                default = { 'lsp', 'path', 'snippets', 'buffer', 'copilot' },
-                providers = {
-                    copilot = {
-                        name = 'copilot',
-                        module = 'blink-copilot',
-                        score_offset = 100,
-                        async = true,
-                    },
-                },
+                default = { 'lsp', 'path', 'snippets', 'buffer' },
             },
             fuzzy = { implementation = "prefer_rust_with_warning" }
         },
