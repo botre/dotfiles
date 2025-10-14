d384d87e1ff62a45f7c0c0e7ee28a1c9519df9e8
botre <git@bjornkrols.com>
2025-10-14 21:54:04 +0200

    Copilot tweaks

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -3,6 +3,8 @@ return {
         'zbirenbaum/copilot.lua',
         cmd = 'Copilot',
         event = 'InsertEnter',
-        opts = {},
+        config = function()
+            require('copilot').setup({})
+        end,
     },
 }
