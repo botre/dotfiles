de8eb9f6afd0cbb94a843bf2fe370095f8975a28
botre <git@bjornkrols.com>
2025-10-19 23:07:21 +0200

    More keys

--- .config/nvim/lua/plugins/buffer-line.lua
+++ .config/nvim/lua/plugins/buffer-line.lua
@@ -10,12 +10,12 @@ return {
                     diagnostics = 'nvim_lsp',
                 },
             })
-            vim.keymap.set('n', '<leader>[t', ':bprevious<CR>')
-            vim.keymap.set('n', '<leader>]t', ':bnext<CR>')
-            vim.keymap.set('n', '<tab>[', ':bprevious<CR>')
-            vim.keymap.set('n', '<tab>]', ':bnext<CR>')
-            vim.keymap.set('n', '<tab>a', ':enew<CR>')
-            vim.keymap.set('n', '<tab>d', ':bdelete<CR>')
+            vim.keymap.set('n', '<leader>[t', ':bprevious<CR>', { desc = 'Previous Tab' })
+            vim.keymap.set('n', '<leader>]t', ':bnext<CR>', { desc = 'Next Tab' })
+            vim.keymap.set('n', '<tab>[', ':bprevious<CR>', { desc = 'Previous Buffer' })
+            vim.keymap.set('n', '<tab>]', ':bnext<CR>', { desc = 'Next Buffer' })
+            vim.keymap.set('n', '<tab>a', ':enew<CR>', { desc = 'New Buffer' })
+            vim.keymap.set('n', '<tab>d', ':bdelete<CR>', { desc = 'Delete Buffer' })
         end,
     },
 }
