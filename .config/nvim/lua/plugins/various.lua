8db4b5330ed2419d419d43be90e65b806b4d9347
botre <git@bjornkrols.com>
2025-10-19 23:10:12 +0200

    Keymaps

--- .config/nvim/lua/plugins/various.lua
+++ .config/nvim/lua/plugins/various.lua
@@ -18,29 +18,36 @@ return {
 
             -- Register key groups
             wk.add({
-                { '<leader>f', group = 'Find' },
-                { '<leader>g', group = 'Go' },
-                { '<leader>r', group = 'Rename/Refactor' },
-                { '<leader>t', group = 'Tree' },
-                { '<leader>[', group = 'Previous' },
-                { '<leader>]', group = 'Next' },
-                { '<leader>w', group = 'Window' },
+                { '<leader>f',   group = 'Find' },
+                { '<leader>g',   group = 'Go' },
+                { '<leader>r',   group = 'Rename/Refactor' },
+                { '<leader>t',   group = 'Tree' },
+                { '<leader>[',   group = 'Previous' },
+                { '<leader>]',   group = 'Next' },
+                { '<leader>w',   group = 'Window' },
 
                 -- Descriptions for .vimrc keymaps
-                { '<leader>C', desc = 'Change to End (Black Hole)' },
-                { '<leader>D', desc = 'Delete to End (Black Hole)' },
-                { '<leader>S', desc = 'Substitute Line (Black Hole)' },
-                { '<leader>X', desc = 'Delete Char Backward (Black Hole)' },
-                { '<leader>c', desc = 'Change (Black Hole)' },
-                { '<leader>d', desc = 'Delete (Black Hole)' },
-                { '<leader>s', desc = 'Substitute (Black Hole)' },
-                { '<leader>x', desc = 'Delete Char (Black Hole)' },
-                { '<leader>h', desc = 'Toggle Highlight Search' },
-                { '<leader>w-', desc = 'Split Horizontal' },
+                { '<leader>C',   desc = 'Change to End (Black Hole)' },
+                { '<leader>D',   desc = 'Delete to End (Black Hole)' },
+                { '<leader>S',   desc = 'Substitute Line (Black Hole)' },
+                { '<leader>X',   desc = 'Delete Char Backward (Black Hole)' },
+                { '<leader>c',   desc = 'Change (Black Hole)' },
+                { '<leader>d',   desc = 'Delete (Black Hole)' },
+                { '<leader>s',   desc = 'Substitute (Black Hole)' },
+                { '<leader>x',   desc = 'Delete Char (Black Hole)' },
+                { '<leader>h',   desc = 'Toggle Highlight Search' },
+                { '<leader>w-',  desc = 'Split Horizontal' },
                 { '<leader>w\\', desc = 'Split Vertical' },
-                { '<leader>n', desc = 'Toggle Relative Numbers' },
+                { '<leader>n',   desc = 'Toggle Relative Numbers' },
             })
         end,
+        keys = {
+            '<leader>?',
+            function()
+                require('which-key').show({ global = false })
+            end,
+            desc = 'Show Keymaps',
+        }
     },
     {
         'kana/vim-textobj-entire',
