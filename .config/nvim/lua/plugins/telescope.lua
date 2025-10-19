ad1545f0ef9e1fc6b82bd8908fc83b2ef185a79b
botre <git@bjornkrols.com>
2025-10-19 22:19:07 +0200

    Tweaks

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -3,6 +3,15 @@ return {
         'nvim-telescope/telescope.nvim',
         dependencies = { 'nvim-lua/plenary.nvim' },
         config = function()
+            require('telescope').setup({
+                defaults = {
+                    file_ignore_patterns = {
+                        ".git/",
+                        "node_modules/",
+                    }
+                }
+            })
+
             local builtin = require('telescope.builtin')
             vim.keymap.set('n', '<leader><space>', builtin.oldfiles, {})
             vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
