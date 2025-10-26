04d2945ccdf276b00931837c16ce1cef8842c73d
botre <git@bjornkrols.com>
2025-10-26 22:19:56 +0100

    bqf

--- .config/nvim/lua/plugins/quickfix.lua
+++ .config/nvim/lua/plugins/quickfix.lua
@@ -1,36 +1,41 @@
 return {
-    'stevearc/quicker.nvim',
-    opts = {},
-    config = function()
-        -- Navigation keymaps
-        vim.keymap.set('n', '[q', ':cprevious<CR>', { desc = 'Previous quickfix item' })
-        vim.keymap.set('n', ']q', ':cnext<CR>', { desc = 'Next quickfix item' })
+    {
+        'stevearc/quicker.nvim',
+        opts = {},
+        config = function()
+            -- Navigation keymaps
+            vim.keymap.set('n', '[q', ':cprevious<CR>', { desc = 'Previous quickfix item' })
+            vim.keymap.set('n', ']q', ':cnext<CR>', { desc = 'Next quickfix item' })
 
-        -- Toggle keymaps
-        vim.keymap.set('n', '<leader>q', function()
-            require('quicker').toggle()
-        end, {
-            desc = 'Toggle quickfix',
-        })
+            -- Toggle keymaps
+            vim.keymap.set('n', '<leader>q', function()
+                require('quicker').toggle()
+            end, {
+                desc = 'Toggle quickfix',
+            })
 
-        -- Setup with expand/collapse keys
-        require('quicker').setup({
-            keys = {
-                {
-                    '>',
-                    function()
-                        require('quicker').expand({ before = 2, after = 2, add_to_existing = true })
-                    end,
-                    desc = 'Expand quickfix context',
+            -- Setup with expand/collapse keys
+            require('quicker').setup({
+                keys = {
+                    {
+                        '>',
+                        function()
+                            require('quicker').expand({ before = 2, after = 2, add_to_existing = true })
+                        end,
+                        desc = 'Expand quickfix context',
+                    },
+                    {
+                        '<',
+                        function()
+                            require('quicker').collapse()
+                        end,
+                        desc = 'Collapse quickfix context',
+                    },
                 },
-                {
-                    '<',
-                    function()
-                        require('quicker').collapse()
-                    end,
-                    desc = 'Collapse quickfix context',
-                },
-            },
-        })
-    end,
+            })
+        end,
+    },
+    {
+        'kevinhwang91/nvim-bqf',
+    }
 }
