b2a2a109935cd040a7ae10eddf839f1e06cb7cd9
botre <git@bjornkrols.com>
2026-05-06 21:29:03 +0200

    Re-enable treesitter for md

--- .config/nvim/lua/plugins/tree-sitter.lua
+++ .config/nvim/lua/plugins/tree-sitter.lua
@@ -33,7 +33,6 @@ return {
                 highlight = {
                     enable = true,
                     additional_vim_regex_highlighting = false,
-                    disable = { 'markdown', 'markdown_inline' },
                 },
             })
         end,
