5fda5be9fb5dd71084d7b8397002bd8c9cafb77a
botre <git@bjornkrols.com>
2026-05-14 11:41:15 +0200

    Migrate nvim-treesitter to main branch for Neovim 0.12

--- .config/nvim/lua/plugins/tree-sitter.lua
+++ .config/nvim/lua/plugins/tree-sitter.lua
@@ -1,55 +1,65 @@
 return {
     {
         'nvim-treesitter/nvim-treesitter',
+        branch = 'main',
         lazy = false,
         build = ':TSUpdate',
         config = function()
-            require('nvim-treesitter.configs').setup({
-                ensure_installed = {
-                    'arduino',
-                    'astro',
-                    'bash',
-                    'c',
-                    'cpp',
-                    'css',
-                    'dockerfile',
-                    'go',
-                    'graphql',
-                    'html',
-                    'javascript',
-                    'json',
-                    'lua',
-                    'markdown',
-                    'markdown_inline',
-                    'python',
-                    'rust',
-                    'toml',
-                    'tsx',
-                    'typescript',
-                    'vim',
-                    'yaml',
-                },
-                sync_install = false,
-                highlight = {
-                    enable = true,
-                },
+            require('nvim-treesitter').install({
+                'arduino',
+                'astro',
+                'bash',
+                'c',
+                'cpp',
+                'css',
+                'dockerfile',
+                'go',
+                'graphql',
+                'html',
+                'javascript',
+                'json',
+                'lua',
+                'markdown',
+                'markdown_inline',
+                'python',
+                'rust',
+                'toml',
+                'tsx',
+                'typescript',
+                'vim',
+                'yaml',
+            })
+
+            vim.api.nvim_create_autocmd('FileType', {
+                callback = function(args)
+                    local ok = pcall(vim.treesitter.start, args.buf)
+                    if ok then
+                        vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+                    end
+                end,
             })
         end,
     },
     {
         'nvim-treesitter/nvim-treesitter-textobjects',
+        branch = 'main',
         dependencies = {
-            'nvim-treesitter/nvim-treesitter'
+            'nvim-treesitter/nvim-treesitter',
         },
         config = function()
-            mover = require('nvim-treesitter.textobjects.move')
-            -- Function Navigation
+            require('nvim-treesitter-textobjects').setup({
+                move = {
+                    set_jumps = true,
+                },
+            })
+
+            local move = require('nvim-treesitter-textobjects.move')
             vim.keymap.set('n', '<leader>[f', function()
-                mover.goto_previous_start('@function.outer')
+                move.goto_previous_start('@function.outer', 'textobjects')
             end, { desc = 'Previous function' })
             vim.keymap.set('n', '<leader>]f', function()
-                mover.goto_next_start('@function.outer')
+                move.goto_next_start('@function.outer', 'textobjects')
             end, { desc = 'Next function' })
-        end
-    }
+        end,
+    },
 }
