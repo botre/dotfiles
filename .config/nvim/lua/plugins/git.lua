75f4deee308f6f7ce2328709a67060c61a4b9551
botre <git@bjornkrols.com>
2026-09-10 20:07:58 +0200

    Migrate telescope, nvim-tree and lazygit.nvim onto snacks.nvim
    Three plugins and their plenary dependency collapse into one. telescope
    still ships releases but the ecosystem has moved on, and lazygit.nvim has
    been quiet since December 2025. nvim-tree is healthy, but snacks.explorer
    is the same picker engine as the finder, so keeping both meant running
    two fuzzy matchers.
    
    Every keymap is preserved: <leader><space>, <leader>ff, <leader>fg,
    <leader>fr and <leader>fu for the picker, <C-t>, <leader>tt and
    <leader>tf for the tree, and <leader>gg for lazygit. <leader>ft is new
    and replaces :TodoTelescope, which died with telescope. One behaviour
    changes: <leader>tf reveals the current file and opens the tree if it is
    closed, where NvimTreeFindFileToggle also closed it again.
    
    snacks.lazygit themes lazygit from the active colorscheme and routes its
    editor back into the running Neovim instance.

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -1,13 +1,6 @@
 return {
     { 'tpope/vim-fugitive' },
     {
-        'kdheepak/lazygit.nvim',
-        dependencies = { 'nvim-lua/plenary.nvim' },
-        config = function()
-            vim.keymap.set('n', '<leader>gg', ':LazyGit<CR>', { desc = 'LazyGit' })
-        end,
-    },
-    {
         'lewis6991/gitsigns.nvim',
         config = function()
             require('gitsigns').setup()
---
+++ .config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,52 @@
+local function explorer_toggle()
+    local explorer = Snacks.picker.get({ source = 'explorer' })[1]
+    if explorer then
+        explorer:close()
+    else
+        Snacks.explorer()
+    end
+end
+
+return {
+    {
+        'folke/snacks.nvim',
+        priority = 1000,
+        lazy = false,
+        opts = {
+            explorer = { enabled = true },
+            lazygit = { enabled = true },
+            picker = {
+                enabled = true,
+                sources = {
+                    -- Carried over from the telescope file_ignore_patterns.
+                    -- fd and rg already skip .gitignore, so these only matter
+                    -- for repositories that track either directory.
+                    files = { exclude = { '.git', 'node_modules' } },
+                    grep = { exclude = { '.git', 'node_modules' } },
+                    -- Show recent files from the current project only
+                    recent = { filter = { cwd = true } },
+                    explorer = {
+                        -- nvim-tree was configured to hide neither dotfiles
+                        -- nor git-ignored files
+                        hidden = true,
+                        ignored = true,
+                    },
+                },
+            },
+        },
+        keys = {
+            { '<leader><space>', function() Snacks.picker.recent() end,         desc = 'Recent files' },
+            { '<leader>ff',      function() Snacks.picker.files() end,          desc = 'Find files' },
+            { '<leader>fg',      function() Snacks.picker.grep() end,           desc = 'Live grep' },
+            { '<leader>fr',      function() Snacks.picker.recent() end,         desc = 'Recent files' },
+            { '<leader>ft',      function() Snacks.picker.todo_comments() end,  desc = 'Find todo comments' },
+            { '<leader>fu',      function() Snacks.picker.lsp_references() end, desc = 'Find references' },
+
+            { '<C-t>',           explorer_toggle,                              desc = 'Tree toggle',    mode = { 'n', 'x' } },
+            { '<leader>tt',      explorer_toggle,                              desc = 'Tree toggle',    mode = { 'n', 'x' } },
+            { '<leader>tf',      function() Snacks.explorer.reveal() end,       desc = 'Reveal in tree', mode = { 'n', 'x' } },
+
+            { '<leader>gg',      function() Snacks.lazygit() end,              desc = 'LazyGit' },
+        },
+    },
+}
